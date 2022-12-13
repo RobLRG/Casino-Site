108e5b54a2b59a5be965fd65a296e0c07936b4ee
robert.peters <bobpeters5889@hotmail.com>
2022-12-13 12:20:23 +0000

    improved visuals, added betting system and gameover screen.

--- script.js
+++ script.js
@@ -45,11 +45,16 @@ const scoreHolder = document.getElementById("scoreContainer")
 const scoreDisplay = document.createElement("div")
 scoreHolder.appendChild(scoreDisplay)
 
+const betHolder = document.getElementById("betContainer")
+const betDisplay = document.createElement("div")
+betHolder.appendChild(betDisplay)
+
 class Player {
     constructor(name) {
         this.name = name
         this.cards = []
         this.score = 0
+        this.money = 1000
     }
 
     draw(deck) {
@@ -86,6 +91,9 @@ class Player {
     }
 }
 
+const moneyDisplay = document.getElementById("money")
+const messageBox = document.getElementById("messageBox")
+
 class Game {
     constructor(player1, player2) {
         this.player1 = player1
@@ -94,6 +102,7 @@ class Game {
     }
 
     setup() {
+        moneyDisplay.innerHTML = this.player1.money
         this.deck.build()
         this.deck.shuffle()
         console.log(this.player1.name)
@@ -113,6 +122,8 @@ class Game {
         if (this.player2.name == "CPU the Destroyer") {
             this.calculateCPUMoves()
         }
+
+        betDisplay.innerHTML = 0
     }
 
     calculateCPUMoves() {
@@ -126,21 +137,28 @@ class Game {
 
 
     findWinner() {
+        messageBox.style.display = "none"
+        while (messageBox.firstChild) {
+            messageBox.removeChild(messageBox.firstChild)
+        }
         let winner;
         let player1Score = this.player1.score
         let player2Score = this.player2.score
 
         if (player1Score > player2Score && player1Score <= 21) {
             winner = this.player1.name
+            this.player1.money = this.player1.money + (parseInt(betDisplay.innerHTML) * 2)
         } else if (player2Score > player1Score && player2Score <= 21) {
             winner = this.player2.name
         } else if (player1Score > 21 && player2Score <= 21) {
             winner = this.player2.name
         } else if (player2Score > 21 && player1Score <= 21) {
             winner = this.player1.name
+            this.player1.money = this.player1.money + (parseInt(betDisplay.innerHTML) * 2)
         }
         else if (player1Score == player2Score || (player1Score > 21 && player2Score > 21)) {
             winner = "noone"
+            this.player1.money = this.player1.money + parseInt(betDisplay.innerHTML)
         }
 
         console.log(`${winner} wins`)
@@ -155,7 +173,6 @@ class Game {
         let dv = document.getElementById("container")
         btn.onclick = () => {
             this.player1.draw(this.deck)
-            // this.player2.draw(this.deck)
         }
         btn.classList.add("button")
         dv.appendChild(btn)
@@ -164,13 +181,32 @@ class Game {
         let text2 = document.createTextNode("Hold")
         btn2.appendChild(text2)
         btn2.onclick = () => {
-            this.findWinner()
+            if (betDisplay.innerHTML != "0") {
+                this.findWinner()
+            } else if (messageBox.style.display == "none") {
+                messageBox.style.display = "block"
+                let message = document.createElement("h1")
+                let text = document.createTextNode("Please input a bet!")
+                message.appendChild(text)
+                messageBox.appendChild(message)
+            }
         }
         btn2.classList.add("button")
         dv.appendChild(btn2)
+
+        let btn3 = document.createElement("button")
+        let text3 = document.createTextNode("add Bet")
+        btn3.appendChild(text3)
+        btn3.onclick = () => {
+            this.addBet()
+        }
+        btn3.classList.add("button")
+        dv.appendChild(btn3)
     }
 
     showEndScreen(winner) {
+        this.turnOverCards()
+
         let container = document.getElementById("finito")
         container.style.display = "block"
         let message = document.createElement("h2")
@@ -201,11 +237,6 @@ class Game {
             btnCont.removeChild(btnCont.firstChild)
         }
 
-        // let scoreCont = document.getElementById("scoreContainer")
-        // while (scoreCont.firstChild) {
-        //     scoreCont.removeChild(scoreCont.firstChild)
-        // }
-
         let cardCont = document.getElementById("cardHolder")
         while (cardCont.firstChild) {
             cardCont.removeChild(cardCont.firstChild)
@@ -217,6 +248,7 @@ class Game {
         }
 
         scoreDisplay.innerHTML = ""
+        betDisplay.innerHTML = 0
 
         let fin = document.getElementById("finito")
 
@@ -225,14 +257,101 @@ class Game {
         }
         fin.style.display = "none"
 
-        this.setup()
+        if (this.player1.money > 0) {
+            this.setup()
+        } else {
+            window.location.href = "gameOver.html"
+        }
+
     }
 
+    turnOverCards() {
+        let oppCards = this.player2.cards
 
 
+        let dv = document.getElementById("oppHolder")
 
+        while (dv.firstChild) {
+            dv.removeChild(dv.firstChild)
+        }
+
+        for (const card of oppCards) {
+            let cardInfo = card.numbers
+            let cardInfoS = card.suits
+
+
+            let cardElement = document.createElement("div")
+            let topnum = document.createElement("h1")
+            let botnum = document.createElement("h1")
+            let suitDisplay = document.createElement("h1")
+
+
+            cardElement.classList.add("card")
+            dv.appendChild(cardElement)
+
+            cardElement.appendChild(topnum)
+            topnum.classList.add("topnum")
+            topnum.innerHTML = cardInfo
+
+            cardElement.appendChild(botnum)
+            botnum.classList.add("botnum")
+            botnum.innerHTML = cardInfo
+
+            cardElement.appendChild(suitDisplay)
+            suitDisplay.classList.add("suit")
+            suitDisplay.innerHTML = cardInfoS
+
+            if ((card.suits == "♥️") || (card.suits == "♦️")) {
+                cardElement.classList.add("redBorder")
+                topnum.classList.add("red")
+                botnum.classList.add("red")
+                suitDisplay.classList.add("red")
+            } else {
+                cardElement.classList.add("blackBorder")
+                topnum.classList.add("black")
+                botnum.classList.add("black")
+                suitDisplay.classList.add("black")
+            }
+
+        }
+
+    }
+
+    addBet() {
+        let container = document.getElementById("bets")
+        container.style.display = "block"
+        let message = document.createElement("h2")
+        message.setAttribute("id", "betMessage")
+        let text = document.createTextNode(`Please choose a bet amount`)
+        message.appendChild(text)
+        container.appendChild(message)
+
+        let input = document.createElement("form")
+        input.setAttribute("id", "form")
+        let inputBox = document.createElement("input")
+        inputBox.setAttribute("type", "text")
+        inputBox.setAttribute("id", "betAmount")
+        inputBox.setAttribute("name", "betAmount")
+        let submitButton = document.createElement("input")
+        submitButton.setAttribute("type", "submit")
+        input.appendChild(inputBox)
+        input.appendChild(submitButton)
+        input.onsubmit = () => {
+            let amount = parseInt(inputBox.value)
+            if (amount <= this.player1.money) {
+                this.player1.money = this.player1.money - amount
+                moneyDisplay.innerHTML = this.player1.money
+                betDisplay.innerHTML = amount
+                container.style.display = "none"
+                while (container.firstChild) {
+                    container.removeChild(container.firstChild)
+                }
+            }
+            return false
+        }
+        container.appendChild(input)
+    }
 }
-
 
 let name = prompt("please enter a name", "human")
 
